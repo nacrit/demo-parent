f88d5c20dc10f75affd917f7fc63a8c16102888a
Mars <zh@alicms.com>
2024-07-05 15:06:58 +0800

    fix

--- demo-spring-boot/src/main/java/com/example/demo/springboot/controller/VueDataController.java
+++ demo-spring-boot/src/main/java/com/example/demo/springboot/controller/VueDataController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
  * @date 2024/6/28 10:21
  */
 @Slf4j
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/vue3/data")
 public class VueDataController {
@@ -35,10 +35,11 @@ public class VueDataController {
 
     private static void initData() throws ParseException {
         USER_LIST.clear();
+        String defaultPass = "123456";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        USER_LIST.add(new User(1L, "Alice", 18, sdf.parse("2024-01-01 00:00:00"), 1, "https://img2.baidu.com/it/u=3868198138,297404319&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"));
-        USER_LIST.add(new User(2L, "Bob", 17, new Date(), 1, "https://img2.baidu.com/it/u=1706107271,3006035273&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400"));
-        USER_LIST.add(new User(3L, "Charlie", 20, sdf.parse("2023-06-01 01:02:03"), 0, "https://q7.itc.cn/q_70/images03/20240423/6d236fae5c8f44ed9b60d977f32debb7.jpeg"));
+        USER_LIST.add(new User(1L, "Alice", defaultPass, 18, sdf.parse("2024-01-01 00:00:00"), 1, "https://img2.baidu.com/it/u=3868198138,297404319&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"));
+        USER_LIST.add(new User(2L, "Bob", defaultPass, 17, new Date(), 1, "https://img2.baidu.com/it/u=1706107271,3006035273&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400"));
+        USER_LIST.add(new User(3L, "Charlie", defaultPass, 20, sdf.parse("2023-06-01 01:02:03"), 0, "https://q7.itc.cn/q_70/images03/20240423/6d236fae5c8f44ed9b60d977f32debb7.jpeg"));
     }
 
     @GetMapping
@@ -46,6 +47,7 @@ public class VueDataController {
         log.debug("[条件查询数据] name={}", name);
         return USER_LIST.stream()
                 .filter(e -> name == null || e.getName().contains(name))
+                .peek(e -> e.setPassword("***"))
                 .collect(Collectors.toList());
     }
 
@@ -59,6 +61,13 @@ public class VueDataController {
             return false;
         }
         return USER_LIST.add(user);
+    }
+
+
+    @PostMapping
+    public Boolean login(@RequestBody User user) {
+        log.info("[登陆用户] user={}", user);
+        return USER_LIST.stream().anyMatch(e -> e.getName().equals(user.getName()) && e.getPassword().equals(user.getPassword()));
     }
 
     @PutMapping
@@ -96,21 +105,10 @@ public class VueDataController {
     public static class User {
         private Long id;
         private String name;
+        private String password;
         private Integer age;
         private Date birthday;
         private Integer status;
         private String image;
-
-        @Override
-        public String toString() {
-            return "User{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", age=" + age +
-                    ", birthday=" + birthday +
-                    ", status=" + status +
-                    ", image='" + image + '\'' +
-                    '}';
-        }
     }
 }
